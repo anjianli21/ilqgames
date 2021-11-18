f36ce28045520b600810a7294a0b8aeb9458d945
Anjian Li <anjianli2001@gmail.com>
2021-11-17 23:53:53 -0500

    first reach avoid, control only quad, use clip

--- python/one_player_reach_avoid.py
+++ python/one_player_reach_avoid.py
@@ -27,14 +27,20 @@ from product_multiplayer_dynamical_system import \
     ProductMultiPlayerDynamicalSystem
 
 from multi_goal_cost import MultiGoalCost
+from reach_avoid_solver import ReachAvoidSolver
 
 # General parameters.
 TIME_HORIZON = 3   # s
 TIME_RESOLUTION = 0.1 # s, default: 0.1
 HORIZON_STEPS = int(TIME_HORIZON / TIME_RESOLUTION)
 LOG_DIRECTORY = "./logs/one_player_dynamic_goal_obs/"
-# MAX_V = 15.0 # m/s
-MAX_V = 15.0 # m/s
+MAX_V = 25.0 # m/s
+MAX_A = 10.0 # m/s^2
+MAX_W = 1.0 # rad/s
+
+# Solver parameter
+alpha_scaling = 0.01
+max_iteration = 400
 
 # Create dynamics.
 car1 = Unicycle4D(T=TIME_RESOLUTION)
@@ -45,26 +51,12 @@ dynamics = ProductMultiPlayerDynamicalSystem(
 theta0 = np.pi / 3 # 60 degree heading
 v0 = 5.0             # 5 m/s initial speed
 
-
-theta0 = np.pi / 3
-v0 = 5.0
-
-# x0 = np.array([[30.0],
-#                [30.0],
-#                [theta0],
-#                [v0]])
-
 # New start
 x0 = np.array([[20.0],
                [20.0],
                [theta0],
                [v0]])
 
-# New start
-x0 = np.array([[20.0],
-               [20.0],
-               [theta0],
-               [v0]])
 
 mult = 0.0
 P1s =  [mult * np.array([[0, 0, 0, 0],
@@ -72,66 +64,14 @@ P1s =  [mult * np.array([[0, 0, 0, 0],
 
 alpha1s = [np.zeros((dynamics._u_dims[0], 1))] * HORIZON_STEPS
 
-
-# Create the example environment. It will have a couple of circular obstacles
-# laid out like this:
-#                           x goal
-#
-#                      ()
-#               ()
-#                            ()
-#
-#          x start
-
 ###################################
-# goal = Point(55.0, 70.0)
-# goal_centers = [Point(50.0, 50.0), Point(40.0, 20.0)]
-# goal_centers = [Point(40.0, 20.0)]
-
-
-# goal_cost = ProximityCost(position_indices=(0, 1),
-#                           point=goal,
-#                           max_distance=np.inf,
-#                           apply_after_time=HORIZON_STEPS - 1,
-#                           # apply_after_time=0,
-#                           name="goal")
-
-# goal_costs = [ProximityCost(position_indices=(0, 1),
-#                             point=p,
-#                             max_distance=np.inf,
-#                             apply_after_time=HORIZON_STEPS - 1,
-#                             # apply_after_time=0,
-#                             name="goal_%f_%f" % (p.x, p.y))
-#               for p in goal_centers]
-
 # Test Multi goal cost
-# goal_centers = [Point(50.0, 50.0), Point(40.0, 20.0)]
-# goal_times = [[28, 29], [14, 15]]
-
-# # Test consecutive goal 1
-# goal_centers = [Point(30.0, 20.0), Point(40.0, 30.0), Point(50, 40)]
+# goal_centers = [Point(30.0, 40.0), Point(40.0, 30.0), Point(50, 20)]
 # goal_times = [[9, 10], [19, 20], [29, 30]]
 
-# Test consecutive goal 2
-# We can try different MAX_V: 15, reach for 2nd goal, 30 reach for 1st goal
-
-goal_centers = [Point(30.0, 40.0), Point(40.0, 30.0), Point(50, 20)]
-goal_times = [[9, 10], [19, 20], [29, 30]]
-# goal_times = [[9, 10], [14, 15], [29, 30]]
-# goal_times = [[29, 30], [14, 15], [9, 10]]
-
-# Only one goal at 20
-# goal_centers = [Point(40.0, 30.0)]
-# goal_times = [[19, 20]]
-
-# Test consecutive goal 3
-# goal_centers = [Point(30.0, 40.0), Point(35, 35), Point(40.0, 30.0), Point(45.0, 25.0), Point(50, 20)]
-# # goal_times = [[0, 10], [10, 15], [15, 20], [20, 30]]
-# goal_times = [[29, 30], [24, 25], [19, 20], [14, 15], [9, 10]]
-
-# # Original goal
-# goal_centers = [Point(55.0, 70.0)]
-# goal_times = [[29, 30]]
+# Test single goal cost
+goal_centers = [Point(50.0, 60.0)]
+goal_times = [[29, 30]]
 
 goal_cost = MultiGoalCost(position_indices=(0, 1),
                           point_list=goal_centers,
@@ -140,27 +80,15 @@ goal_cost = MultiGoalCost(position_indices=(0, 1),
                           name="goal")
 
 ###################################
-# Original obstacles
-# obstacle_centers = [Point(80.0, 15.0),
-#                     Point(45.0, 45.0), Point(15.0, 60.0)]
-# obstacle_radii = [8.0, 8.0, 8.0]
-# obstacle_time = [[0, 30], [0, 30], [0, 30]]
-
 # Current test obstacles
-obstacle_centers = [Point(30, 30), Point(40, 40), Point(50, 30)]
-obstacle_radii = [6.0, 6.0, 6.0]
-obstacle_time = [[0, 10], [10, 20], [20, 30]]
-# obstacle_time = [[0, 20], [10, 20], [20, 30]]
-
-
-# for center in range(30, 60, 10):
-#     obstacle_centers.append(Point(center+0.0, 80.0 - center))
-#     obstacle_radii.append(6.0)
+# obstacle_centers = [Point(30, 30), Point(40, 40), Point(50, 30)]
+# obstacle_radii = [6.0, 6.0, 6.0]
 # obstacle_time = [[0, 10], [10, 20], [20, 30]]
 
-# obstacle_centers = [Point(45.0, 45.0)]
-# obstacle_radii = [8.0]
-
+# Single obstacles
+obstacle_centers = [Point(40, 40)]
+obstacle_radii = [10.0]
+obstacle_time = [[0, 30]]
 
 obstacle_costs = [ObstacleCost(
     position_indices=(0, 1), point=p, max_distance=r,
@@ -170,9 +98,10 @@ obstacle_costs = [ObstacleCost(
 
 w_cost = QuadraticCost(dimension=0, origin=0, name="w_cost")
 a_cost = QuadraticCost(dimension=1, origin=0, name="a_cost")
-
-dvx_cost = QuadraticCost(dimension=0, origin=0, name="dvx_cost")
-dvy_cost = QuadraticCost(dimension=1, origin=0, name="dvy_cost")
+w_cost_upper = SemiquadraticCost(dimension=0, threshold=MAX_W, oriented_right=True, name="w_cost_upper")
+w_cost_lower = SemiquadraticCost(dimension=0, threshold=-MAX_W, oriented_right=False, name="w_cost_lower")
+a_cost_upper = SemiquadraticCost(dimension=1, threshold=MAX_A, oriented_right=True, name="a_cost_upper")
+a_cost_lower = SemiquadraticCost(dimension=1, threshold=-MAX_A, oriented_right=False, name="a_cost_lower")
 
 # Add light quadratic around original values for theta/v.
 v_cost_upper = SemiquadraticCost(
@@ -180,17 +109,13 @@ v_cost_upper = SemiquadraticCost(
 v_cost_lower = SemiquadraticCost(
     dimension=3, threshold=0, oriented_right=False, name="v_cost_lower")
 
-# control constraint
-player1_constraint = BoxConstraint(np.array([[0]]).T, np.array([[MAX_V]]).T)
 
 # OBSTACLE_WEIGHT = 100.0 # HJI: 100
 OBSTACLE_WEIGHT = 200.0 # HJI: 100
 GOAL_WEIGHT = 100.0 # HJI: 400
-D_WEIGHT = 1000.0 # HJI: 1000
 U_WEIGHT = 1.0 # HJI: 100
 
-V_WEIGHT = 1.0
-# V_WEIGHT = 50.0 # HJI: 50
+V_WEIGHT = 100.0
 
 # Build up total costs for both players. This is basically a zero-sum game.
 player1_cost = PlayerCost()
@@ -202,6 +127,13 @@ player1_cost.add_cost(v_cost_upper, "x", V_WEIGHT)
 player1_cost.add_cost(v_cost_lower, "x", V_WEIGHT)
 player1_cost.add_cost(w_cost, 0, U_WEIGHT)
 player1_cost.add_cost(a_cost, 0, U_WEIGHT)
+# player1_cost.add_cost(a_cost_upper, 0, U_WEIGHT)
+# player1_cost.add_cost(a_cost_lower, 0, U_WEIGHT)
+# player1_cost.add_cost(w_cost_upper, 0, U_WEIGHT)
+# player1_cost.add_cost(w_cost_lower, 0, U_WEIGHT)
+#################################
+# Control constraints
+player1_u_constraint = BoxConstraint(np.array([[-MAX_W, -MAX_A]]).T, np.array([[MAX_W, MAX_A]]).T)
 
 
 # Visualizer.
@@ -226,19 +158,18 @@ print("Saving log file to {}...".format(path_to_logfile))
 logger = Logger(path_to_logfile)
 
 # Set up ILQSolver.
-solver = ILQSolver(dynamics,
-                   [player1_cost],
-                   x0,
-                   [P1s],
-                   [alpha1s],
-                   # 0.01, # 0.01
-                   0.1,
-                   200,
-                   None,
-                   logger,
-                   visualizer,
-                   # u_constraints=[player1_constraint],
-                   TOLERANCE_PERCENTAGE=1e-4)
+solver = ReachAvoidSolver(dynamics,
+                        [player1_cost],
+                        x0,
+                        [P1s],
+                        [alpha1s],
+                        alpha_scaling,
+                        max_iteration,
+                        None,
+                        logger,
+                        visualizer,
+                        u_constraints=[player1_u_constraint],
+                        TOLERANCE_PERCENTAGE=1e-7)
 
 def handle_sigint(sig, frame):
     print("SIGINT caught! Saving log data...")
